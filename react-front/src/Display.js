8310320a14c20ad27af16361e946fa7ebb2676d1
harry thomas <harryt1905@gmail.com>
2022-12-05 18:56:41 -0500

    missing braket

--- react-front/src/Display.js
+++ react-front/src/Display.js
@@ -16,42 +16,42 @@ export default function Display({ subs, setHovered, reccomendedOrder, reccomende
 
     const chartRef = useRef();
 
-  const onClick = (event) => {
-    if (getElementsAtEvent(chartRef.current, event).length > 0) {
-      const datasetIndexNum = getElementsAtEvent(chartRef.current, event)[0].datasetIndex;
-      const dataPoint = getElementsAtEvent(chartRef.current, event)[0].index;
-      console.log(`Dataset: ${datasetIndexNum} and  Data: ${dataPoint} `);
-      console.log(data.datasets[datasetIndexNum].link[dataPoint]);
-      // console.log(event.target.value)
-      // console.log(event)
+    const onClick = (event) => {
+        if (getElementsAtEvent(chartRef.current, event).length > 0) {
+            const datasetIndexNum = getElementsAtEvent(chartRef.current, event)[0].datasetIndex;
+            const dataPoint = getElementsAtEvent(chartRef.current, event)[0].index;
+            console.log(`Dataset: ${datasetIndexNum} and  Data: ${dataPoint} `);
+            console.log(data.datasets[datasetIndexNum].link[dataPoint]);
+            // console.log(event.target.value)
+            // console.log(event)
+        }
     }
+    const countCategories = () => {
+        let categories = [];
+        for (let i = 0; i < subs.length; i++) {
+            const allSubsValues = Object.values(subs[i].topicDetails.mainCategories);
+            categories.push(allSubsValues);
+        }
+        const count = categories.flat().reduce((acc, curr) => (acc[curr] = (acc[curr] || 0) + 1, acc), {});
+        return count;
+    };
 
-  const countCategories = () => {
-    let categories = [];
-    for (let i = 0; i < subs.length; i++) {
-      const allSubsValues = Object.values(subs[i].topicDetails.mainCategories);
-      categories.push(allSubsValues);
-    }
-    const count = categories.flat().reduce((acc, curr) => (acc[curr] = (acc[curr] || 0) + 1, acc), {});
-    return count;
-  };
-
-  const colorObject = { Gaming: 'rgb(255, 255, 0)', Lifestyle: 'rgb(0, 128, 0)', Music: 'rgb(255, 0, 0)', Society: 'rgb(0, 0, 255)', Entertainment: 'rgb(255, 165, 0)', Technology: 'rgb(0, 255, 102)', Hobby: 'rgb(193, 193, 193)', Sports: 'rgb(207, 37, 190)' };
+    const colorObject = { Gaming: 'rgb(255, 255, 0)', Lifestyle: 'rgb(0, 128, 0)', Music: 'rgb(255, 0, 0)', Society: 'rgb(0, 0, 255)', Entertainment: 'rgb(255, 165, 0)', Technology: 'rgb(0, 255, 102)', Hobby: 'rgb(193, 193, 193)', Sports: 'rgb(207, 37, 190)' };
 
 
-  const data = {
-    labels: Object.keys(countCategories(subs)),
-    datasets: [{
-      label: 'My Categories',
-      data: Object.values(countCategories(subs)),
-      backgroundColor:
-        Object.keys(countCategories(subs)).map((item) => {
-          return colorObject[item];
-        })
-      ,
-      hoverOffset: 4,
-      link: Object.keys(countCategories(subs))
-    }],
+    const data = {
+        labels: Object.keys(countCategories(subs)),
+        datasets: [{
+            label: 'My Categories',
+            data: Object.values(countCategories(subs)),
+            backgroundColor:
+                Object.keys(countCategories(subs)).map((item) => {
+                    return colorObject[item];
+                })
+            ,
+            hoverOffset: 4,
+            link: Object.keys(countCategories(subs))
+        }],
 
     };
 
